90663dfedc0cd020ef26eafc5d6bd3a83c40c153
mx <max@maxvl.com>
2023-03-29 23:38:02 -0400

    Add main function to instantiate a load-tester, create a few scenarios and execute them

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,31 @@
-package main 
+package main
 
+import (
+	"github.com/maxlvl/gocust/internal/client"
+	"github.com/maxlvl/gocust/internal/loadtester"
+	"time"
+)
 
 func main() {
+	ltConfig := loadtester.LoadTesterConfig{
+		Concurrency:  5,
+		TestDuration: 10 * time.Second,
+		HTTPClientConfig: client.HTTPClientConfig{
+			Timeout: 3 * time.Second,
+		},
+	}
+
+	lt := loadtester.NewLoadTester(ltConfig)
+
+	scenarios := []loadtester.Scenario{
+		&loadtester.SimpleScenario{URL: "http://example.com"},
+		&loadtester.ComplexScenario{
+			GetURL:  "http://example.com",
+			PostURL: "http://example.com/post",
+			Payload: map[string]string{
+				"key": "value",
+			},
+		},
+	}
+	lt.Run(scenarios)
 }
