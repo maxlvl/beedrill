551098d61116b5c5ad4206b1effab46071950c4b
mx <max@maxvl.com>
2023-03-29 22:19:20 -0400

    Add test for Post

--- internal/client/httpclient_test.go
+++ internal/client/httpclient_test.go
@@ -10,7 +10,7 @@ import (
   "github.com/stretchr/testify/assert"
 )
 
-func TestHTTPClient(t *testing.T) {
+func TestHTTPClientGet(t *testing.T) {
   ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
     w.Write([]byte("OK"))
@@ -24,6 +24,25 @@ func TestHTTPClient(t *testing.T) {
   httpClient := NewHTTPClient(config)
 
   body, err := Get(httpClient, ts.URL)
+  assert.NoError(t, err)
+  assert.Equal(t, "OK", body)
+}
+
+func TestHTTPClientPost(t *testing.T) {
+  ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
+  }))
+  defer ts.Close()
+
+  config := HTTPClientConfig{
+    Timeout: 5 * time.Second,
+  }
+
+  httpClient := NewHTTPClient(config)
+  payload := "{'hello': 'world'}"
+
+  body, err := Post(httpClient, ts.URL, payload)
   assert.NoError(t, err)
   assert.Equal(t, "OK", body)
 }
