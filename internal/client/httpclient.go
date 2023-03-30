17f072f7cf3793683cfd4e45c2e7c2ea9c58ebab
mx <max@maxvl.com>
2023-03-29 22:07:35 -0400

    Fix up httpclient by not overriding httpclient methods + fix formatting directives in log statements

--- internal/client/httpclient.go
+++ internal/client/httpclient.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+  "bytes"
 	"net/http"
 	"io/ioutil"
 	"time"
@@ -18,31 +19,36 @@ func NewHTTPClient(config HTTPClientConfig) *http.Client {
 	}
 }
 
-func (c *http.Client) Get(url string) (string, error) {
-  resp, err := c.Get(url)
+func Get(httpclient *http.Client, url string) (string, error) {
+  resp, err := httpclient.Get(url)
   if err != nil {
-    log.Fatal("Error GET url: %s", err)
+    log.Fatalf("Error GET url: %s", err)
+    return "", err
   }
   defer resp.Body.Close()
 
   body, err := ioutil.ReadAll(resp.Body)
   if err != nil {
-    log.Fatal("Error reading response body on get request url: %s", err)
+    log.Fatalf("Error reading response body on get request url: %s", err)
+    return "", err
   }
 
-  return string(body)
+  return string(body), nil
 }
 
-func (c *http.Client) Post(url string, body string) (string, error) {
-	resp, err := c.Post(url, "application/json", body)
+func Post(httpclient *http.Client, url string, payload string) (string, error) {
+  payloadBuffer := bytes.NewBufferString(payload)
+	resp, err := httpclient.Post(url, "application/json", payloadBuffer)
 	if err != nil {
-    log.Fatal("Error POST url: %s", err)
+    log.Fatalf("Error POST url: %s", err)
+    return "", err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-    log.Fatal("Error reading response body on POST url: %s", err)
+    log.Fatalf("Error reading response body on POST url: %s", err)
+    return "", err
 	}
 
 	return string(body), nil
