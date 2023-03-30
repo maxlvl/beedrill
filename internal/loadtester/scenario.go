40965064666c157f81cbb66fa1cdf691f7818ced
mx <max@maxvl.com>
2023-03-29 23:37:44 -0400

    Add simple and complex example scenarios

--- internal/loadtester/scenario.go
+++ internal/loadtester/scenario.go
@@ -1,1 +1,28 @@
 package loadtester
+
+import (
+  "encoding/json"
+  "net/http"
+	"github.com/maxlvl/gocust/internal/client"
+)
+
+type SimpleScenario struct {
+  URL     string
+}
+
+func (s *SimpleScenario) Execute(httpClient *http.Client) {
+  client.Get(httpClient, s.URL)
+}
+
+
+type ComplexScenario struct {
+  GetURL       string
+  PostURL      string
+  Payload      interface{}
+}
+
+func (c *ComplexScenario) Execute(httpClient *http.Client) {
+  httpClient.Get(c.GetURL)
+  data, _ := json.Marshal(c.Payload)
+  client.Post(httpClient, c.PostURL, string(data))
+}
