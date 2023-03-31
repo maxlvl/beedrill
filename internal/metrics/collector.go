a00d43f1d18bd6e11aacedecc3b0f24d9455f1b0
mx <max@maxvl.com>
2023-03-30 22:37:57 -0400

    Add metrics collector

--- internal/metrics/collector.go
+++ internal/metrics/collector.go
@@ -1,1 +1,23 @@
 package metrics
+
+import "github.com/maxlvl/gocust/internal/loadtester"
+
+type Collector struct {
+  Results chan loadtester.Result
+}
+
+func NewCollector() *Collector {
+  return &Collector{
+    Results: make(chan loadtester.Result),
+  }
+}
+
+func (c *Collector) Collect(result loadtester.Result) {
+  c.Results <- result
+}
+
+func (c *Collector) Close() {
+  reporter := metrics.NewReporter(c)
+  reporter.Report()
+  close(c.Results)
+}
