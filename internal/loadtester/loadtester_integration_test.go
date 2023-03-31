9429749caef3e8e733d5df9d50bfb858a9964a80
mx <max@maxvl.com>
2023-03-30 21:47:35 -0400

    Add loadtester integration test with simpleScenario

---
+++ internal/loadtester/loadtester_integration_test.go
@@ -0,0 +1,37 @@
+package loadtester
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"sync/atomic"
+	"testing"
+	"time"
+	"github.com/stretchr/testify/assert"
+	"github.com/maxlvl/gocust/internal/client"
+)
+
+func TestLoadTesterIntegration(t *testing.T) {
+  var requestCounter int32
+
+  ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    atomic.AddInt32(&requestCounter, 1)
+    w.WriteHeader(http.StatusOK)
+  }))
+  
+  defer ts.Close()
+
+  config := LoadTesterConfig{
+    Concurrency:        2,
+    TestDuration:       500 * time.Millisecond,
+    HTTPClientConfig:   client.HTTPClientConfig{
+      Timeout: 3 * time.Second,
+    },
+  }
+
+  lt := NewLoadTester(config)
+
+  scenarios := []Scenario{&SimpleScenario{URL: ts.URL}}
+
+  lt.Run(scenarios)
+  assert.Greater(t, int(atomic.LoadInt32(&requestCounter)), 0, "load tester should've incremented the counter at least once")
+}
