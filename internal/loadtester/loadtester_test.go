2017fd308c2a289b476249355649f80bc10bf843
mx <max@maxvl.com>
2023-03-30 20:56:51 -0400

    Add loadtester Run test

---
+++ internal/loadtester/loadtester_test.go
@@ -0,0 +1,36 @@
+package loadtester
+
+import (
+  "testing"
+  "time"
+  "net/http"
+  "github.com/maxlvl/gocust/internal/client"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockScenario struct {}
+
+func (ms *MockScenario) Execute(httpclient *http.Client) {
+  time.Sleep(100 * time.Millisecond)
+}
+
+func TestLoadTester_Run(t *testing.T) {
+  config := LoadTesterConfig{
+    Concurrency:        2,
+    TestDuration:       500 * time.Millisecond,
+    HTTPClientConfig:   client.HTTPClientConfig{
+      Timeout: 3 * time.Second,
+    },
+  }
+
+  lt := NewLoadTester(config)
+
+  startTime := time.Now()
+  scenarios := []Scenario{&MockScenario{}}
+  lt.Run(scenarios)
+  elapsed := time.Since(startTime)
+
+  assert.GreaterOrEqual(t, int64(elapsed), int64(500 * time.Millisecond))
+  assert.LessOrEqual(t, int64(elapsed), int64(600 * time.Millisecond))
+
+}
