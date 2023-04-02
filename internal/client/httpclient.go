a67e93da0b07344ef0a001a0e0933ddd932e39a1
mx <max@maxvl.com>
2023-04-01 23:17:54 -0400

    Remove httpclient debug messages

--- internal/client/httpclient.go
+++ internal/client/httpclient.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-  "fmt"
   "bytes"
 	"net/http"
 	"time"
@@ -20,7 +19,6 @@ func NewHTTPClient(config HTTPClientConfig) *http.Client {
 
 func Get(httpclient *http.Client, url string) (*http.Response, error) {
   resp, err := httpclient.Get(url)
-  fmt.Printf("Sending GET request to %s\n", url)
   if err != nil {
     log.Fatalf("Error GET url: %s", err)
     return nil, err
@@ -31,7 +29,6 @@ func Get(httpclient *http.Client, url string) (*http.Response, error) {
 
 func Post(httpclient *http.Client, url string, payload string) (*http.Response, error) {
   payloadBuffer := bytes.NewBufferString(payload)
-  fmt.Printf("Sending POST request to %s\n", url)
 	resp, err := httpclient.Post(url, "application/json", payloadBuffer)
 	if err != nil {
     log.Fatalf("Error POST url: %s", err)
