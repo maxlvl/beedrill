0c8c1b5f013441343de9d6869478d3a685a9eec5
mx <max@maxvl.com>
2023-04-01 22:34:32 -0400

    update httpclient's get and post requests

--- internal/client/httpclient.go
+++ internal/client/httpclient.go
@@ -4,7 +4,6 @@ import (
   "fmt"
   "bytes"
 	"net/http"
-	"io/ioutil"
 	"time"
   "log"
 )
@@ -19,39 +18,24 @@ func NewHTTPClient(config HTTPClientConfig) *http.Client {
 	}
 }
 
-func Get(httpclient *http.Client, url string) (string, error) {
+func Get(httpclient *http.Client, url string) (*http.Response, error) {
   resp, err := httpclient.Get(url)
   fmt.Printf("Sending GET request to %s\n", url)
   if err != nil {
     log.Fatalf("Error GET url: %s", err)
-    return "", err
-  }
-  defer resp.Body.Close()
-
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    log.Fatalf("Error reading response body on get request url: %s", err)
-    return "", err
+    return nil, err
   }
 
-  return string(body), nil
+  return resp, nil
 }
 
-func Post(httpclient *http.Client, url string, payload string) (string, error) {
+func Post(httpclient *http.Client, url string, payload string) (*http.Response, error) {
   payloadBuffer := bytes.NewBufferString(payload)
   fmt.Printf("Sending POST request to %s\n", url)
 	resp, err := httpclient.Post(url, "application/json", payloadBuffer)
 	if err != nil {
     log.Fatalf("Error POST url: %s", err)
-    return "", err
+    return nil, err
 	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-    log.Fatalf("Error reading response body on POST url: %s", err)
-    return "", err
-	}
-
-	return string(body), nil
+	return resp, nil
 }
