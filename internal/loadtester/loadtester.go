d1d2011bd6359e94ba7152d58fa62923d7bfbeb9
mx <max@maxvl.com>
2023-04-03 20:25:01 -0400

    update loadtester package with new metrics path and formatting

--- internal/loadtester/loadtester.go
+++ internal/loadtester/loadtester.go
@@ -1,65 +1,64 @@
 package loadtester
 
 import (
-  "sync"
-  "fmt"
-  "time"
+	"fmt"
+	"github.com/maxlvl/gocust/internal/client"
+	"github.com/maxlvl/gocust/internal/metrics"
+	"github.com/maxlvl/gocust/scenarios"
+	"math/rand"
 	"net/http"
-  "math/rand"
-  "github.com/maxlvl/gocust/internal/client"
-  "github.com/maxlvl/gocust/scenarios"
+	"sync"
+	"time"
 )
 
-
 type LoadTesterConfig struct {
-  Concurrency       int
-  TestDuration      time.Duration
-  HTTPClientConfig  client.HTTPClientConfig 
+	Concurrency      int
+	TestDuration     time.Duration
+	HTTPClientConfig client.HTTPClientConfig
 }
 
 type LoadTester struct {
-  config        LoadTesterConfig  
-  httpClient    *http.Client
-  collector     *Collector
+	config     LoadTesterConfig
+	httpClient *http.Client
+	collector  *metrics.Collector
 }
 
 func NewLoadTester(config LoadTesterConfig) *LoadTester {
-  return &LoadTester{
-    config: config,
-    httpClient: client.NewHTTPClient(config.HTTPClientConfig),
-    collector: NewCollector(),
-  }
+	return &LoadTester{
+		config:     config,
+		httpClient: client.NewHTTPClient(config.HTTPClientConfig),
+		collector:  metrics.NewCollector(),
+	}
 }
 
 func (lt *LoadTester) Run(scenarios []scenarios.Scenario) {
-  // no need for channels yet as each goroutine can run independently
-  // will likely introduce channels once we need aggregated metrics for all goroutines
-  var waitGroup sync.WaitGroup
-  fmt.Printf("Adding %d concurrent workers...\n", lt.config.Concurrency)
-  waitGroup.Add(lt.config.Concurrency)
+	// no need for channels yet as each goroutine can run independently
+	// will likely introduce channels once we need aggregated metrics for all goroutines
+	var waitGroup sync.WaitGroup
+	fmt.Printf("Adding %d concurrent workers...\n", lt.config.Concurrency)
+	waitGroup.Add(lt.config.Concurrency)
 
-  fmt.Printf("Test duration: %s\n", lt.config.TestDuration) // Print TestDuration
+	fmt.Printf("Test duration: %s\n", lt.config.TestDuration) // Print TestDuration
 
-  for i := 0; i < lt.config.Concurrency; i++ {
-    go func() {
-      defer waitGroup.Done()
-      startTime := time.Now()
+	for i := 0; i < lt.config.Concurrency; i++ {
+		go func() {
+			defer waitGroup.Done()
+			startTime := time.Now()
 
-      for time.Since(startTime) < lt.config.TestDuration {
-        elapsedTime := time.Since(startTime)
-        fmt.Printf("Running random scenario for startTime %s, elapsed time: %s\n", startTime.Format("2006-01-02 15:04:05"), elapsedTime) // Print elapsed time
-        scenarioIndex := rand.Intn(len(scenarios))
-        result, err := scenarios[scenarioIndex].Execute(lt.httpClient)
-        if err != nil {
-          fmt.Printf("Error running Scenario: %s", err)
-        }
-        lt.collector.Collect(*result)
-      }
-    }()
-  }
+			for time.Since(startTime) < lt.config.TestDuration {
+				elapsedTime := time.Since(startTime)
+				fmt.Printf("Running random scenario for startTime %s, elapsed time: %s\n", startTime.Format("2006-01-02 15:04:05"), elapsedTime) // Print elapsed time
+				scenarioIndex := rand.Intn(len(scenarios))
+				result, err := scenarios[scenarioIndex].Execute(lt.httpClient)
+				if err != nil {
+					fmt.Printf("Error running Scenario: %s", err)
+				}
+				lt.collector.Collect(*result)
+			}
+		}()
+	}
 
-  waitGroup.Wait()
-  reporter := NewReporter(lt.collector)
-  reporter.Report()
+	waitGroup.Wait()
+	reporter := metrics.NewReporter(lt.collector)
+	reporter.Report()
 }
-
