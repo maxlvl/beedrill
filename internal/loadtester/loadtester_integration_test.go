37977058cb7ac56d6c6ccf674ca6df8123c8c2b5
mx <max@maxvl.com>
2023-04-03 20:25:06 -0400

    formatting loadtester tests

--- internal/loadtester/loadtester_integration_test.go
+++ internal/loadtester/loadtester_integration_test.go
@@ -1,39 +1,38 @@
 package loadtester
 
 import (
+	"github.com/maxlvl/gocust/internal/client"
+	"github.com/maxlvl/gocust/scenarios"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"sync/atomic"
 	"testing"
 	"time"
-	"github.com/stretchr/testify/assert"
-	"github.com/maxlvl/gocust/internal/client"
-	"github.com/maxlvl/gocust/scenarios"
-
 )
 
 func TestLoadTesterIntegration(t *testing.T) {
-  var requestCounter int32
+	var requestCounter int32
 
-  ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    atomic.AddInt32(&requestCounter, 1)
-    w.WriteHeader(http.StatusOK)
-  }))
-  
-  defer ts.Close()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		atomic.AddInt32(&requestCounter, 1)
+		w.WriteHeader(http.StatusOK)
+	}))
 
-  config := LoadTesterConfig{
-    Concurrency:        2,
-    TestDuration:       500 * time.Millisecond,
-    HTTPClientConfig:   client.HTTPClientConfig{
-      Timeout: 3 * time.Second,
-    },
-  }
+	defer ts.Close()
 
-  lt := NewLoadTester(config)
+	config := LoadTesterConfig{
+		Concurrency:  2,
+		TestDuration: 500 * time.Millisecond,
+		HTTPClientConfig: client.HTTPClientConfig{
+			Timeout: 3 * time.Second,
+		},
+	}
 
-  scenarios := []scenarios.Scenario{&scenarios.SimpleScenario{URL: ts.URL}}
+	lt := NewLoadTester(config)
 
-  lt.Run(scenarios)
-  assert.Greater(t, int(atomic.LoadInt32(&requestCounter)), 0, "load tester should've incremented the counter at least once")
+	scenarios := []scenarios.Scenario{&scenarios.SimpleScenario{URL: ts.URL}}
+
+	lt.Run(scenarios)
+	assert.Greater(t, int(atomic.LoadInt32(&requestCounter)), 0, "load tester should've incremented the counter at least once")
 }
--- internal/loadtester/loadtester_test.go
+++ internal/loadtester/loadtester_test.go
@@ -1,39 +1,39 @@
 package loadtester
 
 import (
-  "testing"
-  "time"
-  "net/http"
-  "github.com/maxlvl/gocust/internal/client"
+	"github.com/maxlvl/gocust/internal/client"
+	"github.com/maxlvl/gocust/internal/result"
+	"github.com/maxlvl/gocust/scenarios"
 	"github.com/stretchr/testify/assert"
-  "github.com/maxlvl/gocust/internal/result"
-  "github.com/maxlvl/gocust/scenarios"
+	"net/http"
+	"testing"
+	"time"
 )
 
-type MockScenario struct {}
+type MockScenario struct{}
 
 func (ms *MockScenario) Execute(httpclient *http.Client) (*result.Result, error) {
-  time.Sleep(100 * time.Millisecond)
-  return &result.Result{}, nil
+	time.Sleep(100 * time.Millisecond)
+	return &result.Result{}, nil
 }
 
 func TestLoadTester_Run(t *testing.T) {
-  config := LoadTesterConfig{
-    Concurrency:        2,
-    TestDuration:       500 * time.Millisecond,
-    HTTPClientConfig:   client.HTTPClientConfig{
-      Timeout: 3 * time.Second,
-    },
-  }
+	config := LoadTesterConfig{
+		Concurrency:  2,
+		TestDuration: 500 * time.Millisecond,
+		HTTPClientConfig: client.HTTPClientConfig{
+			Timeout: 3 * time.Second,
+		},
+	}
 
-  lt := NewLoadTester(config)
+	lt := NewLoadTester(config)
 
-  startTime := time.Now()
-  scenarios := []scenarios.Scenario{&MockScenario{}}
-  lt.Run(scenarios)
-  elapsed := time.Since(startTime)
+	startTime := time.Now()
+	scenarios := []scenarios.Scenario{&MockScenario{}}
+	lt.Run(scenarios)
+	elapsed := time.Since(startTime)
 
-  assert.GreaterOrEqual(t, int64(elapsed), int64(500 * time.Millisecond))
-  assert.LessOrEqual(t, int64(elapsed), int64(600 * time.Millisecond))
+	assert.GreaterOrEqual(t, int64(elapsed), int64(500*time.Millisecond))
+	assert.LessOrEqual(t, int64(elapsed), int64(600*time.Millisecond))
 
 }
