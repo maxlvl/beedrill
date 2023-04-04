6e51c3e08fc0b1249ce58dcfbdccc9fbd2d95beb
mx <max@maxvl.com>
2023-04-03 20:24:18 -0400

    fmt

--- internal/client/httpclient.go
+++ internal/client/httpclient.go
@@ -1,10 +1,10 @@
 package client
 
 import (
-  "bytes"
+	"bytes"
+	"log"
 	"net/http"
 	"time"
-  "log"
 )
 
 type HTTPClientConfig struct {
@@ -18,21 +18,21 @@ func NewHTTPClient(config HTTPClientConfig) *http.Client {
 }
 
 func Get(httpclient *http.Client, url string) (*http.Response, error) {
-  resp, err := httpclient.Get(url)
-  if err != nil {
-    log.Fatalf("Error GET url: %s", err)
-    return nil, err
-  }
+	resp, err := httpclient.Get(url)
+	if err != nil {
+		log.Fatalf("Error GET url: %s", err)
+		return nil, err
+	}
 
-  return resp, nil
+	return resp, nil
 }
 
 func Post(httpclient *http.Client, url string, payload string) (*http.Response, error) {
-  payloadBuffer := bytes.NewBufferString(payload)
+	payloadBuffer := bytes.NewBufferString(payload)
 	resp, err := httpclient.Post(url, "application/json", payloadBuffer)
 	if err != nil {
-    log.Fatalf("Error POST url: %s", err)
-    return nil, err
+		log.Fatalf("Error POST url: %s", err)
+		return nil, err
 	}
 	return resp, nil
 }
--- internal/client/httpclient_test.go
+++ internal/client/httpclient_test.go
@@ -1,48 +1,47 @@
 package client
 
-
 import (
-  "net/http"
-  "net/http/httptest"
-  "testing"
-  "time"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
 
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHTTPClientGet(t *testing.T) {
-  ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("OK"))
-  }))
-  defer ts.Close()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}))
+	defer ts.Close()
 
-  config := HTTPClientConfig{
-    Timeout: 5 * time.Second,
-  }
+	config := HTTPClientConfig{
+		Timeout: 5 * time.Second,
+	}
 
-  httpClient := NewHTTPClient(config)
+	httpClient := NewHTTPClient(config)
 
-  resp, err := Get(httpClient, ts.URL)
-  assert.NoError(t, err)
-  assert.Equal(t, 200, resp.StatusCode)
+	resp, err := Get(httpClient, ts.URL)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
 }
 
 func TestHTTPClientPost(t *testing.T) {
-  ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("OK"))
-  }))
-  defer ts.Close()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}))
+	defer ts.Close()
 
-  config := HTTPClientConfig{
-    Timeout: 5 * time.Second,
-  }
+	config := HTTPClientConfig{
+		Timeout: 5 * time.Second,
+	}
 
-  httpClient := NewHTTPClient(config)
-  payload := "{'hello': 'world'}"
+	httpClient := NewHTTPClient(config)
+	payload := "{'hello': 'world'}"
 
-  resp, err := Post(httpClient, ts.URL, payload)
-  assert.NoError(t, err)
-  assert.Equal(t, 200, resp.StatusCode)
+	resp, err := Post(httpClient, ts.URL, payload)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
 }
