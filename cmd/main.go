f3f929ff885c4389246eb7734e52a2ed38c7d33b
mx <max@maxvl.com>
2023-04-05 20:28:41 -0400

    Inject config into server constructor + update main.go to no longer run the LT

--- cmd/main.go
+++ cmd/main.go
@@ -2,29 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/maxlvl/gocust/internal/client"
+	"github.com/maxlvl/gocust/config"
 	"github.com/maxlvl/gocust/internal/loadtester"
-	"github.com/maxlvl/gocust/scenarios"
 	"github.com/maxlvl/gocust/web/server"
-	"gopkg.in/yaml.v3"
+	yaml "gopkg.in/yaml.v3"
 	"io/ioutil"
-	"time"
 )
-
-type Config struct {
-	Concurrency      int                     `yaml:"concurrency"`
-	TestDuration     time.Duration           `yaml:"test_duration"`
-	HTTPClientConfig client.HTTPClientConfig `yaml:"http_client_config"`
-	Scenarios        []ScenarioConfig        `yaml:"scenarios"`
-}
-
-type ScenarioConfig struct {
-	Type    string            `yaml:"type"`
-	URL     string            `yaml:"url,omitempty"`
-	GetURL  string            `yaml:"get_url,omitempty"`
-	PostURL string            `yaml:"post_url,omitempty"`
-	Payload map[string]string `yaml:"payload,omitempty"`
-}
 
 func main() {
 	configData, err := ioutil.ReadFile("../config.yaml")
@@ -33,42 +16,25 @@ func main() {
 		return
 	}
 
-	var config Config
-	err = yaml.Unmarshal(configData, &config)
+	var cfg config.Config
+	err = yaml.Unmarshal(configData, &cfg)
 	if err != nil {
 		fmt.Sprintf("Error unmarshalling config: %s\n", err)
 		return
 	}
 
 	ltConfig := loadtester.LoadTesterConfig{
-		Concurrency:      config.Concurrency,
-		TestDuration:     config.TestDuration,
-		HTTPClientConfig: config.HTTPClientConfig,
+		Concurrency:      cfg.Concurrency,
+		TestDuration:     cfg.TestDuration,
+		HTTPClientConfig: cfg.HTTPClientConfig,
 	}
 
 	lt := loadtester.NewLoadTester(ltConfig)
 
-	srv := web.NewServer(":8080", lt)
+	srv := web.NewServer(":8080", lt, cfg)
 	err = srv.Start()
 	if err != nil {
 		fmt.Sprintf("Error starting webserver : %s\n", err)
 		return
 	}
-
-	var scenarios_array []scenarios.Scenario
-	for _, s := range config.Scenarios {
-		switch s.Type {
-		case "simple":
-			scenarios_array = append(scenarios_array, &scenarios.SimpleScenario{URL: s.URL})
-		case "complex":
-			scenarios_array = append(
-				scenarios_array,
-				&scenarios.ComplexScenario{
-					GetURL:  s.GetURL,
-					PostURL: s.PostURL,
-					Payload: s.Payload,
-				})
-		}
-	}
-	lt.Run(scenarios_array)
 }
--- web/server/server.go
+++ web/server/server.go
@@ -1,31 +1,59 @@
 package web
 
 import (
+	"fmt"
+	"github.com/maxlvl/gocust/config"
 	"github.com/maxlvl/gocust/internal/loadtester"
+	"github.com/maxlvl/gocust/scenarios"
 	"net/http"
 )
 
 type Server struct {
 	httpServer *http.Server
 	lt         *loadtester.LoadTester
+	config_    config.Config
 }
 
-func NewServer(addr string, lt *loadtester.LoadTester) *Server {
+func NewServer(addr string, lt *loadtester.LoadTester, config_ config.Config) *Server {
 	return &Server{
 		httpServer: &http.Server{
 			Addr: addr,
 		},
-		lt: lt,
+		lt:      lt,
+		config_: config_,
 	}
 }
 
 func (s *Server) Start() error {
 	http.HandleFunc("/", s.HandleIndex)
-	// http.HandleFunc('/api/v1/start', s.HandleStart(),
+	http.HandleFunc("/api/v1/start", s.HandleStart)
 	// http.HandleFunc('/api/v1/stop', s.HandleStop(),
-	return s.httpServer.ListenAndServe()
+	s.httpServer.ListenAndServe()
+	return nil
 }
 
 func (s *Server) HandleIndex(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "../index.html")
+}
+
+func (s *Server) HandleStart(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("In the HandleStart func")
+	// TODO: figure out how to actually start the LT run from here
+	var scenarios_array []scenarios.Scenario
+	for _, s := range s.config_.Scenarios {
+		switch s.Type {
+		case "simple":
+			scenarios_array = append(scenarios_array, &scenarios.SimpleScenario{URL: s.URL})
+		case "complex":
+			scenarios_array = append(
+				scenarios_array,
+				&scenarios.ComplexScenario{
+					GetURL:  s.GetURL,
+					PostURL: s.PostURL,
+					Payload: s.Payload,
+				})
+		}
+	}
+	s.lt.Run(scenarios_array)
+
 }
